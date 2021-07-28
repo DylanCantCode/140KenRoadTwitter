0204a1c2e91c76f9ffe11e29039f83a4d0780b20
Dylan <dylan.lea@mh.ox.ac.uk>
2021-07-28 15:44:00 +0100

    added intelligence

---
+++ ai.py
@@ -0,0 +1,24 @@
+import openai
+import config
+import json
+
+def getFakeTweet(friend):
+    f = open("friends/{}.json".format(friend["name"]), "r")
+    tweets = json.load(f)
+    f.close()
+    input = "User: " + "\nUser: ".join(tweets) + "\nUser: "
+
+    openai.api_key = config.openai_key
+
+    response = openai.Completion.create(
+      engine="davinci",
+      prompt=input,
+      temperature=0.75,
+      max_tokens=42,
+      top_p=1,
+      frequency_penalty=1,
+      presence_penalty=0
+    )
+
+    clean_response = response["choices"][0]["text"].split("\n")[0]
+    return(clean_response)
--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import tweepy
 import config
 from process import processTweet
 from friends import readFriends, updateFriends, getTweets
+from ai import getFakeTweet
 
 auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
